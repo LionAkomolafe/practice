c1eaa10a958c4f62bc8acd6fb83b36258297e0d1
Yemi <tywonderkid@yahoo.com>
2023-04-27 10:34:41 -0500

    Adding Shell Topics

---
+++ roboshop-shell/mongodb.sh
@@ -0,0 +1,13 @@
+yum install mongodb-org -y
+systemctl enable mongod
+systemctl start mongod
+
+sed -i -e 's/127.0.0.0/0.0.0.0' /etc/mongod.conf
+systemctl restart mongod
+
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+cd /tmp
+unzip mongodb.zip
+cd mongodb-main
+mongo < catalogue.js
+mongo < users.js
